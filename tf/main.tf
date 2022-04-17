8cc4ed287af9318bf29a79ac4e02dc8dd6d4e7a7
Erekle Magradze <erekle@magradze.de>
2022-04-17 12:36:20 +0400

    adding main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -5,3 +5,12 @@ resource "docker_image" "backend" {
         dockerfile = "Dockerfile"
     }
 }
+
+resource "aws_ecr_repository" "foo" {
+  name                 = "backendimagerepo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
